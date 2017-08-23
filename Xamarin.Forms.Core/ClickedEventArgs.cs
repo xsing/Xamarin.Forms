f7cded576b6167311c8f6110551a3d4bb535ad25
Rui Marinho <me@ruimarinho.net>
2017-08-23 16:22:40 +0100

    [Core] Add ClickedGestureREcognizer

---
+++ Xamarin.Forms.Core/ClickedEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Xamarin.Forms
+{
+	public class ClickedEventArgs : EventArgs
+	{
+		public ClickedEventArgs(object commandParameter)
+		{
+			Parameter = commandParameter;
+		}
+
+		public int Buttons
+		{
+			get;
+			set;
+		}
+
+		public object Parameter { get; private set; }
+	}
+}
